af1855c69f842660b743d4a770efe146f051693c
gladishd <gladish.dean@gmail.com>
2022-05-30 13:04:34 -0400

    changed file names

--- client/components/Chat.js
+++ client/components/Chat.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import Sidebar from './Sidebar'
-import Navbar from './Navbar'
+import Navbar from './navbar'
 import MessagesList from './MessagesList'
 
 export default class Main extends Component {
